0b3a9e849e04af9310d5ec23e7e3c1078b5cd69a
Martyn Russell <martyn@lanedo.com>
2013-08-13 19:30:14 +0100

    build: Remove gettext requirement

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-#
-# NOTE: compare_versions() is stolen from gnome-autogen.sh
 
 # Generate required files
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
-  cd "$srcdir" &&
-  touch ChangeLog && # Automake requires that ChangeLog exist
-  autopoint --force &&
-  AUTOPOINT='intltoolize --automake --copy' autoreconf --verbose --force --install
+  touch ChangeLog # Required by automake.
+  autoreconf --verbose --force --install
 ) || exit
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
+# Run configure
+"$srcdir/configure" "$@"
